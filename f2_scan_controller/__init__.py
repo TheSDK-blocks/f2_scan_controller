84f9625d752fe0af09e9bab20e64a8b700aa4d5f
Marko Kosunen <marko.kosunen@aalto.fi>
2019-02-27 22:04:04 -0800

    First version of test memory control

--- f2_scan_controller/__init__.py
+++ f2_scan_controller/__init__.py
@@ -20,18 +20,43 @@ class f2_scan_controller(verilog,thesdk):
 
     def __init__(self,*arg): 
         self.proplist = [ 'Rs', 
+                          'Rs_dsp'
                           'Rxantennas', 
-                          'rx_output_mode' 
+                          'rx_output_mode', 
+                          'rx_dsp_mode', 
+                          'dsp_interpolator_scales',
+                          'dsp_decimator_scales',
+                          'dsp_interpolator_cic3shift',   
+                          'dsp_decimator_cic3shift',
+                          'nserdes',
+                          'neighbours'
                         ];    # properties that can be propagated from parent
-        self.Rs = 1;                 # sampling frequency
-        self.Txantennas = 4;                 # sampling frequency
-        self.Rxantennas = 4;                 # sampling frequency
+        self.Rs = 160e6;                 # Highest sampling frequency at 
+        self.Rs_dsp = 20e6;              # dsp sampling frequency
+        self.Txantennas = 4;             # Number of antennas
+        self.Rxantennas = 4;             # Nuber of antenna
+        self.Users      = 4;             # Users
+        self.nserdes    = 2;             # Serdeses
+        self.neighbours = 2;             # neighbours
+        self.rx_output_mode = 1;
+        self.rx_dsp_mode = 4; #Log2(decimratio) or 4
+        self.dsp_interpolator_scales=[1,1,1,1]
+        self.dsp_interpolator_cic3shift=4
+        self.dsp_decimator_scales=[1,1,1,1]
+        self.dsp_decimator_cic3shift=4
         self.model='py';             # can be set externally, but is not propagated
         self.par= False              # By default, no parallel processing
         self.queue= []               # By default, no parallel processing
         # We now where the verilog file is. 
         # Let's read in the file to have IOs defined
         self.dut=verilog_module(file=self.entitypath + '/../f2_dsp/sv/f2_dsp.sv')
+        # We need to replicate this in order to get witdth automatically
+        self.clockdivider=verilog_module(file=self.entitypath +'/../f2_dsp/sv/clkdiv_n_2_4_8.v')
+        self.clockdivider.io_signals.mv(fro='io_Ndiv',to='lane_refclk_Ndiv')
+        self.clockdivider.io_signals.mv(fro='io_shift',to='lane_refclk_shift')
+        self.clockdivider.io_signals.mv(fro='io_clkpn',to='lane_refclk')
+        self.clockdivider.io_signals.mv(fro='io_reset_clk',to='lane_refclk_reset')
+        self.clockdivider.io_signals.mv(fro='reset',to='reset_clock_div')
 
         # Scan ins the way to pass the controls# 
         # Format: Time in rows, 
@@ -44,12 +69,162 @@ class f2_scan_controller(verilog,thesdk):
         # These are signals of tb driving several targets
         # Not present in DUT
         self.connectors=verilog_connector_bundle()
-        
 
         if len(arg)>=1:
             parent=arg[0]
             self.copy_propval(parent,self.proplist)
             self.parent =parent;
+
+        #These are signals not in dut
+        self.newsigs=[
+                 'initdone',
+                 'reset_loop'
+                ]
+        # Initialize Selected signals with parameter values
+        # These are tuples defining name init value pair
+        self.signallist=[
+                ('reset', 1),
+                ('reset_loop',1),
+                ('initdone',0),
+                ('reset_clock_div',1),
+                ('lane_refclk_reset',1),
+                ('io_ctrl_and_clocks_tx_reset_clkdiv', 1),
+                ('io_ctrl_and_clocks_rx_reset_clkdiv', 1),
+                ('io_ctrl_and_clocks_reset_dacfifo', 1),
+                ('io_ctrl_and_clocks_reset_outfifo', 1),
+                ('io_ctrl_and_clocks_reset_infifo', 1),
+                ('io_ctrl_and_clocks_reset_adcfifo', 1),
+                ('lane_refclk_Ndiv', 2),
+                ('lane_refclk_shift', 0),
+                ('io_ctrl_and_clocks_tx_Ndiv', int(self.Rs/(8*self.Rs_dsp))),
+                ('io_ctrl_and_clocks_tx_clkdiv_shift', 0),
+                ('io_ctrl_and_clocks_rx_Ndiv', int(self.Rs/(8*self.Rs_dsp))),
+                ('io_ctrl_and_clocks_rx_clkdiv_shift', 0),
+                ('io_ctrl_and_clocks_user_spread_mode', 0),
+                ('io_ctrl_and_clocks_user_index', 0),
+                ('io_ctrl_and_clocks_antenna_index', 0),
+                ('io_ctrl_and_clocks_input_mode', 0),
+                ('io_ctrl_and_clocks_adc_fifo_lut_mode', 2),
+                ('io_ctrl_and_clocks_rx_output_mode', self.rx_output_mode),
+                ('io_ctrl_and_clocks_serdestest_scan_write_mode', 0),
+                ('io_ctrl_and_clocks_serdestest_scan_write_address', 0),
+                ('io_ctrl_and_clocks_serdestest_scan_write_en', 0),
+                ('io_ctrl_and_clocks_serdestest_scan_read_mode', 0),
+                ('io_ctrl_and_clocks_serdestest_scan_read_address', 0),
+                ('io_ctrl_and_clocks_serdestest_scan_read_en', 0)
+            ]
+        for user in range(self.Users):
+            self.signallist+=[ 
+                ('io_ctrl_and_clocks_serdestest_scan_write_value_data_%s_udata_real' \
+                            %(user), 0), 
+                ('io_ctrl_and_clocks_serdestest_scan_write_value_data_%s_udata_imag' \
+                        %(user), 0) 
+            ]
+                # This is scan output. No bidirectional scan yet
+                #('io_ctrl_and_clocks_serdestest_scan_write_value' 0),
+        for tx in range(self.Txantennas):
+            self.signallist+=[
+                ('io_ctrl_and_clocks_interpolator_controls_%s_cic3derivscale' %(tx), 
+                    self.dsp_interpolator_scales[3]),
+                ('io_ctrl_and_clocks_interpolator_controls_%s_cic3derivshift' %(tx), 
+                    self.dsp_interpolator_cic3shift),
+                ('io_ctrl_and_clocks_interpolator_controls_%s_hb1scale' %(tx), 
+                    self.dsp_interpolator_scales[0]),
+                ('io_ctrl_and_clocks_interpolator_controls_%s_hb2scale' %(tx), 
+                    self.dsp_interpolator_scales[1]),
+                ('io_ctrl_and_clocks_interpolator_controls_%s_hb3scale' %(tx), 
+                    self.dsp_interpolator_scales[2]),
+                ('io_ctrl_and_clocks_interpolator_controls_%s_mode' %(tx), 
+                    4),
+                ('io_ctrl_and_clocks_user_sum_mode_%s' %(tx), 0),
+                ('io_ctrl_and_clocks_user_select_index_%s' %(tx), 0),
+                ('io_ctrl_and_clocks_dac_data_mode_%s' %(tx), 6),
+                ('io_ctrl_and_clocks_dac_lut_write_en_%s' %(tx), 0),
+                ('io_ctrl_and_clocks_dac_lut_write_addr_%s' %(tx), 0),
+                ('io_ctrl_and_clocks_dac_lut_write_vals_%s_real' %(tx), 0),
+                ('io_ctrl_and_clocks_dac_lut_write_vals_%s_imag' %(tx), 0)
+            ]
+            for user in range(self.Users):
+                self.signallist+=[
+                    ('io_ctrl_and_clocks_tx_user_weights_%s_%s_real' %(tx,user),1),
+                    ('io_ctrl_and_clocks_tx_user_weights_%s_%s_imag' %(tx,user),1)
+                ]
+
+
+        for rx in range(self.Rxantennas):
+            self.signallist+=[
+                ('io_ctrl_and_clocks_decimator_controls_%s_cic3integscale' %(rx), 
+                    self.dsp_decimator_scales[0]),
+                ('io_ctrl_and_clocks_decimator_controls_%s_cic3integshift' %(rx), 
+                    self.dsp_decimator_cic3shift),
+                ('io_ctrl_and_clocks_decimator_controls_%s_hb1scale' %(rx), 
+                    self.dsp_decimator_scales[1]),
+                ('io_ctrl_and_clocks_decimator_controls_%s_hb2scale' %(rx), 
+                    self.dsp_decimator_scales[2]),
+                ('io_ctrl_and_clocks_decimator_controls_%s_hb3scale' %(rx), 
+                    self.dsp_decimator_scales[3]),
+                ('io_ctrl_and_clocks_decimator_controls_%s_mode' %(rx), 
+                    self.rx_dsp_mode),
+                ('io_ctrl_and_clocks_inv_adc_clk_pol_%s' %(rx), 1),
+                ('io_ctrl_and_clocks_adc_lut_write_vals_%s_real' %(rx), 0),
+                ('io_ctrl_and_clocks_adc_lut_write_vals_%s_imag' %(rx), 0)
+            ]
+            for user in range(self.Users):
+                self.signallist+=[
+                    ('io_ctrl_and_clocks_rx_user_weights_%s_%s_real' %(rx,user),1),
+                    ('io_ctrl_and_clocks_rx_user_weights_%s_%s_imag' %(rx,user),1)
+                ]
+
+        self.signallist+=[
+            ('io_ctrl_and_clocks_adc_lut_reset',1),
+            ('io_ctrl_and_clocks_adc_lut_write_en',0),
+            ('io_ctrl_and_clocks_adc_lut_write_addr',0)
+        ]
+
+        #Every 
+        #dsp_to_serdes_address   = Vec(numserdes+2,Input(UInt(log2Ceil(neighbours+2).W)))
+        #serdes_to_dsp_address   = Vec(neighbours+2,Input(UInt(log2Ceil(numserdes+2).W)))
+        # from_dsp 0, from rx_dsp
+        # from_dsp 1-neighbours from tx_dsp outputs to neighbours
+        # from_dsp neighbours+1 from serdestest memory
+
+        # from_serdes( 0 to numseres-1) from serdes rx
+        # from_serdes(numserdes) from rx_dsp
+        # from_serdes(numserdes+1) from serdestest_memory
+
+        # to dsp (0) to tx_dsp_iptr_A
+        # to dsp (1-neighbours) to rx_dsp_neighbours
+        # to dsp (neighbours+1) to serdestest_memory
+        
+        # To serdes has only nserdes valid addresses
+        # to_serdes(0 to nserdes-1) io.lanes_tx(0 to nserdes-1)
+
+        #To connect to memory the output of the serdes0_rx
+        #serdes_to_dsp_address(neighbours+1)=0
+
+        #To connect to memory the output of the rx_dsp 
+        # serdes_to_dsp_address(neighbours+1)=numserdes
+
+
+        #To connect to serdes0_tx the output of the rx_dsp
+        #dsp_to_serdes_address(0)=0
+        #To connect to serdes0_tx the output of the memory
+        #dsp_to_serdes_address(0)=neighbours+1
+        #To connect to serdes0_tx the output of the tx_neighbour output1
+        #dsp_to_serdes_address(0)=1
+
+
+        for scanind in range(self.nserdes+2):
+            self.signallist+=[ 
+                ('io_ctrl_and_clocks_from_serdes_scan_%s_valid' %(scanind), 1),
+                ('io_ctrl_and_clocks_from_dsp_scan_%s_valid' %(scanind),1),
+                ('io_ctrl_and_clocks_to_serdes_mode_%s' %(scanind), 1),
+                ('io_ctrl_and_clocks_dsp_to_serdes_address_%s' %(scanind), 0),
+                ('io_ctrl_and_clocks_serdes_to_dsp_address_%s' %(scanind), 0)
+            ]
+        for scanind in range(self.nserdes+4):
+            self.signallist+=[ ('io_ctrl_and_clocks_to_dsp_mode_%s' %(scanind), 1)]
+
         self.init()
 
     def init(self):
@@ -66,70 +241,53 @@ class f2_scan_controller(verilog,thesdk):
         self.reset()
         self.init_dac_lut()
         self.init_adc_lut()
-
+        self.setup()
+        
     # First we start to control Verilog simulations with 
     # This controller. I.e we pass the IOfile definition
     def setup(self,**kwargs):
-        mode=kwargs.get(mode,'default')
+        #mode=kwargs.get('mode','default')
+        mode=kwargs.get('mode','serdes_rx_to_memory')
+        print('Mode is %s' %(mode))
+        f=self._scan.Data.Members['scan_inputs']
         if mode=='default':
-            pass
-        elif mode=='rx_outmode':
-            pass
+            f.set_control_data(time=self.curr_time,name='initdone', val=1)
+        elif mode=='serdes_rx_to_memory':
+            f.set_control_data(time=self.curr_time,name='initdone', val=1)
+            #To connect to memory the output of the serdes0_rx
+            #serdes_to_dsp_address(neighbours+1)=0
+            f.set_control_data(time=self.curr_time,name\
+                ='io_ctrl_and_clocks_serdes_to_dsp_address_%s' %(self.neighbours+1),val=0)
+            #Mode2 is to fill
+            f.set_control_data(time=self.curr_time,name\
+                    ='io_ctrl_and_clocks_serdestest_scan_write_mode', val=2)
 
     def define_scan(self):
         # This is a bit complex way of passing the data,
         # But eventually we pass only the data , not the file
         # Definition. File should be created in the testbench
-        newsigs=['reset_loop',
-                 'reset_clock_div',
-                 'lane_refclk_reset'
-                ]
-        for name in newsigs:
+        scansigs=[]
+        for name, val in self.signallist:
             # We manipulate connectors as verilog_iofile operate on those
-            self.connectors.new(name=name, cls='reg') 
-
-        # These are dut signals
-        dutsigs=[
-            'reset',
-            'io_ctrl_and_clocks_tx_reset_clkdiv',
-            'io_ctrl_and_clocks_rx_reset_clkdiv',
-            'io_ctrl_and_clocks_reset_dacfifo',
-            'io_ctrl_and_clocks_reset_outfifo',
-            'io_ctrl_and_clocks_reset_infifo',
-            'io_ctrl_and_clocks_reset_adcfifo'
-        ]
-        for name in dutsigs:
-            #i Reveals if those signals even exist.
-            self.connectors.Members[name]=self.dut.io_signals.Members[name] 
-            self.connectors.Members[name].init=''
-        self.scansigs=newsigs+dutsigs
-
-        dutsigs=[]
-        for i in range(self.Txantennas):
-            dutsigs.append('io_ctrl_and_clocks_dac_lut_write_en_%s' %(i))
-            dutsigs.append('io_ctrl_and_clocks_dac_lut_write_addr_%s' %(i))
-            dutsigs.append('io_ctrl_and_clocks_dac_lut_write_vals_%s_real' %(i))
-            dutsigs.append('io_ctrl_and_clocks_dac_lut_write_vals_%s_imag' %(i))
-
-        for name in dutsigs:
-            self.connectors.Members[name]=self.dut.io_signals.Members[name] 
-            self.connectors.Members[name].init=''
-        self.scansigs+=dutsigs
-
-        dutsigs=['io_ctrl_and_clocks_adc_lut_write_en']
-        dutsigs.append('io_ctrl_and_clocks_adc_lut_write_addr')
-        for i in range(self.Txantennas):
-            dutsigs.append('io_ctrl_and_clocks_adc_lut_write_vals_%s_real' %(i))
-            dutsigs.append('io_ctrl_and_clocks_adc_lut_write_vals_%s_imag' %(i))
-        for name in dutsigs:
-            self.connectors.Members[name]=self.dut.io_signals.Members[name] 
-            self.connectors.Members[name].init=''
-
-        self.scansigs+=dutsigs
-
+            if name in self.newsigs:
+                self.connectors.new(name=name, cls='reg')
+            elif name in [
+                'lane_refclk_Ndiv',
+                'lane_refclk_shift',
+                'lane_refclk',
+                'lane_refclk_reset',
+                'reset_clock_div']:
+                self.connectors.Members[name]=\
+                        self.clockdivider.io_signals.Members[name] 
+            else:
+                self.connectors.Members[name]=self.dut.io_signals.Members[name] 
+                self.connectors.Members[name].init=''
+            scansigs.append(name) 
         f=self._scan.Data.Members['scan_inputs']
-        f.verilog_connectors=self.connectors.list(names=self.scansigs)
+        f.verilog_connectors=self.connectors.list(names=scansigs)
         f.set_control_data(init=0) # Initialize to zeros at time 0
+        for name, val in self.signallist:
+            f.set_control_data(time=0,name=name,val=val) 
 
     def reset(self):
         #start defining the file
@@ -178,15 +336,15 @@ class f2_scan_controller(verilog,thesdk):
                       'reset_loop', 
                       ]:
             f.set_control_data(time=time,name=name,val=0)
+        self.curr_time=time
 
     def init_dac_lut(self):
         #Define the signals
-        time_offset=int(32*64/(self.Rs*1e-12))
         step=int(16/(self.Rs*1e-12))
         # Let's assign the shorthand for the iofile
         f=self._scan.Data.Members['scan_inputs']
         memaddrcount=0;
-        time=time_offset
+        time=self.curr_time
         while (memaddrcount<2**9):
             #This is really controlled by Scan, but we do not have scan model
             for index in range(self.Txantennas): 
@@ -202,10 +360,11 @@ class f2_scan_controller(verilog,thesdk):
             time+=step
             for index in range(self.Txantennas): 
                 f.set_control_data(time=time,name='io_ctrl_and_clocks_dac_lut_write_en_%s' %(index),val=0)
+        self.curr_time=time
 
     def init_adc_lut(self):
         #Define the signals
-        time_offset=int(32*64/(self.Rs*1e-12))
+        time_offset=self.curr_time
         step=int(16/(self.Rs*1e-12))
         # Let's assign the shorthand for the iofile
         f=self._scan.Data.Members['scan_inputs']
@@ -220,9 +379,13 @@ class f2_scan_controller(verilog,thesdk):
             memaddrcount+=1
             time+=step
             f.set_control_data(time=time,name='io_ctrl_and_clocks_adc_lut_write_en', val=0)
+            f.set_control_data(time=time,name='io_ctrl_and_clocks_adc_lut_reset', val=0)
+        self.curr_time=time
 
-    def serdestest(self):
+        
+    def test_rx_through_serdestest(self):
         pass
+
   #output        io_ctrl_and_clocks_from_serdes_scan_0_ready, // @[:@123372.4]
   #input         io_ctrl_and_clocks_from_serdes_scan_0_valid, // @[:@123372.4]
   #input  [15:0] io_ctrl_and_clocks_from_serdes_scan_0_bits_data_0_udata_real, // @[:@123372.4]
@@ -659,7 +822,6 @@ class f2_scan_controller(verilog,thesdk):
         # read_address 
         # read_value   
         # read_en      
-        pass
         #name='serdestest_write'
         #ionames=[]
         #ionames+=['io_ctrl_and_clocks_serdestest_scan_write_mode',
